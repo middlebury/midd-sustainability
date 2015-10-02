9947e7e619f784cc1a8894f1a36ebec47c3a567a
Zeb Pykosz <zeb.apy@gmail.com>
2015-10-02 16:51:07 -0400

    wrap function in dom ready because script is loaded in head

--- src/js/sustainability.js
+++ src/js/sustainability.js
@@ -1,8 +1,8 @@
-
-    $('.menu-btn').on('click', function(e) {
-        e.preventDefault();
-        $('#midd-menu').toggleClass('menu-open')
 (function($) {
+    $(document).ready(function() {
+        $('.menu-btn').on('click', function(e) {
+            e.preventDefault();
+            $('#midd-menu').toggleClass('menu-open')
+        });
     });
-
 })(jQuery);
